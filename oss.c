1bfbb7f16cdc8686b146f4f58ebd670368b7b65c
Syed Tariq <syed.tariq1988@gmail.com>
2019-02-24 06:07:31 +0500

    feature: add shared memory variabel and initialize

--- oss.c
+++ oss.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 
 void read_args(int argc, char **argv,int *h, int *n, int *s, char **i, char **o){
 	int j = 0;
@@ -29,5 +32,17 @@ int main(int argc, char **argv){
 	int s = 2;
 	char *i = "input.txt";
 	char *o = "output.txt";
+	key_t svar1;
+	int svarid1;
+	key_t svar2;
+	int svarid2;
+	svar1 = ftok(".", 'x');
+	svar2 = ftok(".", 'y');
+	svarid1 = shmget(svar1, sizeof(int), IPC_CREAT | 0666);
+	svarid2 = shmget(svar2, sizeof(int), IPC_CREAT | 0666);
 	read_args(argc, argv, &h, &n, &s, &i, &o);
+	int *svarptr1 = (int *)shmat(svarid1, NULL, 0);
+	int *svarptr2 = (int *)shmat(svarid2, NULL, 0);
+	*svarptr1 = 0;
+	*svarptr1 = 0;
 }
